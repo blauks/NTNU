38b112a283667ebcd6c893bbbdd9351d2f598027
blauks <brooks.jonathan1@gmail.com>
2018-09-24 14:50:44 +0200

    1234

--- ITGK/test.py
+++ ITGK/test.py
@@ -1,27 +1,9 @@
-########################
-#import random 
-#randomtall = random.randint(0,100)
-#print(randomtall)
-#brukerTall = -1
-#while brukerTall != randomtall:
-    #brukerTall = int(input("Gjett et tall mellom 1 og 100 "))
-    #if brukerTall == randomtall:
-    #    print("Du klarte det! Tallet var:",randomtall)
-   # elif brukerTall < randomtall:
-  #      print("For lavt!")
- #   else:
-#        print("For høyt!")
-#########################
+sum = 0
 
-##############################
-#for x in range(0,31,3):
- #   print(x, x**2)
-#    x = x+1
-##############################
+def funk(a):
+    sum = a * a
+    print(a,"*",a,"=",sum)
 
-##############3
-for y in range(1,4):
-    for x in range(1,4):
-        z = x *y
-print(z)
-##########
+
+while True:
+    funk(int(input("Tall: ")))
