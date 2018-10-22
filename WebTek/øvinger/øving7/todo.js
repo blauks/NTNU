891cdae8d0bbcb4f0017ae87261372ffb4240143
blauks <brooks.jonathan1@gmail.com>
2018-10-22 14:06:33 +0200

    test

--- WebTek/øvinger/øving7/todo.js
+++ WebTek/øvinger/øving7/todo.js
@@ -5,9 +5,20 @@ const todoList = document.getElementById("list")
 var tasks = []
 
 function addTask(){
-    let todo = textField.value   
-    if(todo != ""){
-        todoList.innerHTML += "<li><input type='checkbox'>"+todo+"</input></li>"
+    let todo = textField.value
+    if(todo != "" && !tasks.includes(todo)){
+        console.log(!tasks.includes(todo))
+        todoList.innerHTML = "<li><input onClick='checkboxFunc(this.name)' name="+todo+" type='checkbox'><label id="+todo+">"+todo+"</label></input></li>" + todoList.innerHTML
+        tasks.push(todo)
+        console.log(tasks)
     }
 }
 
+function checkboxFunc(eId){ //eId = element ID
+    if(document.getElementById(eId).style.textDecoration != "line-through"){
+        document.getElementById(eId).style.textDecoration = "line-through"
+    }
+    else{
+        document.getElementById(eId).style.textDecoration = "none"   
+    }
+} //Did not manage to create a variable for document.getElementById(eId).style.textDecoration
