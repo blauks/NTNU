b72aa0f954547020c3496f80dbd80a5f8fb75c0d
blauks <brooks.jonathan1@gmail.com>
2018-10-24 14:37:29 +0200

    idk

--- WebTek/øvinger/øving7/todo.js
+++ WebTek/øvinger/øving7/todo.js
@@ -2,23 +2,45 @@ const textField = document.getElementById("textInput")
 const addButton = document.getElementById("buttonAdd")
 const todoList = document.getElementById("list")
 
-var tasks = []
+
+
+var tasks = [] //I use this list to check if the task already has been added and only store the todo names
+var tasksAndDate = [] //To store the task and the variable from the now function
+
+//I decided to add another list variabel because it only having the names makes it easier to use in my code,
+//and the tasksAndDate could be useful if I ever want to check when it was made
 
 function addTask(){
     let todo = textField.value
     if(todo != "" && !tasks.includes(todo)){
-        console.log(!tasks.includes(todo))
         todoList.innerHTML = "<li><input onClick='checkboxFunc(this.name)' name="+todo+" type='checkbox'><label id="+todo+">"+todo+"</label></input></li>" + todoList.innerHTML
         tasks.push(todo)
-        console.log(tasks)
+        tasksAndDate.push(todo,Date.now())
+        textField.value = "" 
     }
+    updateCompletedlist()
+    console.log(tasks,":",tasksAndDate)
 }
 
-function checkboxFunc(eId){ //eId = element ID
-    if(document.getElementById(eId).style.textDecoration != "line-through"){
-        document.getElementById(eId).style.textDecoration = "line-through"
-    }
+function checkboxFunc(lId){ //lId = the label inside the list elements ID
+    if(document.getElementById(lId).style.textDecoration != "line-through"){
+        document.getElementById(lId).style.textDecoration = "line-through"
+    }   
     else{
-        document.getElementById(eId).style.textDecoration = "none"   
+        document.getElementById(lId).style.textDecoration = "none"   
     }
+    updateCompletedlist()
 } //Did not manage to create a variable for document.getElementById(eId).style.textDecoration
+
+function updateCompletedlist(){
+    let howMany = 0
+    for(x in tasks){
+       if(document.getElementById(tasks[x]).style.textDecoration == "line-through"){
+            howMany = howMany + 1
+       }
+    }
+    document.getElementById("completed").innerHTML = howMany+"/"+tasks.length+" completed"
+}
+
+//small bug with my program is that when you add a new task to your list and one of the checkboxes in the list
+//are already checked the checkbox will uncheck but the task will keep its line through
