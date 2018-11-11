dd1d15cb8b4a5b21f1b66b92778dab8f97244e62
blauks <brooks.jonathan1@gmail.com>
2018-11-11 21:23:53 +0100

    idk

--- ITGK/forelesning øving/5.11.2018/oppgaver.py
+++ ITGK/forelesning øving/5.11.2018/oppgaver.py
@@ -6,3 +6,18 @@ def seqSearch(lst, item):
     return False
 
 print(seqSearch(["a","b","c","d"],"x"))
+
+def binSearch(liste, verdi, imin, imax):
+    if imax < imin:
+        return "ikke funnet verdi"
+    else:
+        midtpunkt = (imin+imax)//2
+        if verdi < liste[midtpunkt]:
+            return binSearch(liste,verdi,imin,midtpunkt-1)
+        elif verdi > liste[midtpunkt]:
+            return binSearch(liste,verdi,midtpunkt+1,imax)
+        else:
+            return midtpunkt
+
+liste = [1,2,3,9,11,13,17,25,57,90]
+print(binSearch(liste,57,0,9))
